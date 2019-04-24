1faaab930b08c0dee14518740df43851c7f45fd9
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2019-04-24 17:46:00 +0200

    initialize python project

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pythonmatsim',
+    version='1.0-SNAPSHOT',
+    packages=['pythonmatsim'],
+    url='',
+    license='GNU GPL 3.0',
+    author='Thibaut Dubernet',
+    author_email='thibaut.dubernet@ivt.baug.ethz.ch',
+    description='A package to use MATSim from Python'
+)
