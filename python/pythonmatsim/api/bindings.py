5f5598cdb5b7719536fc60f81de5c7edf0184fb9
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2019-07-04 15:44:26 +0200

    remove useless file

--- python/pythonmatsim/api/bindings.py
+++
@@ -1,9 +0,0 @@
-from jpype import *
-
-# This assumes the JVM is already started
-
-_org = JPackage('org')
-
-Controler = _org.matsim.core.controler.Controler
-Scenario = _org.matsim.core.scenario.Scenario
-Config = _org.matsim.core.config.Config
