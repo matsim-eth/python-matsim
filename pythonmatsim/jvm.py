a195f6f8a0734febb3aeeb1b4d559114bfe8411f
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2019-07-17 12:51:13 +0200

    do not fail if start_jvm() is called several times

--- pythonmatsim/jvm.py
+++ pythonmatsim/jvm.py
@@ -28,10 +28,14 @@ _logger = logging.getLogger(__name__)
 def start_jvm(jvm_path=jpype.get_default_jvm_path(),
               *additional_classpath):
     if not pkg_resources.resource_exists('javaresources', 'python-matsim-instance-1.0-SNAPSHOT-jar-with-dependencies.jar'):
-        raise Exception('could not find jar file')
+        raise RuntimeError('could not find jar file')
+
+    if jpype.isJVMStarted():
+        # TODO: check that classpath etc. are the ones we want
+        _logger.info("JVM is already live, do nothing.")
+        return
 
     python_matsim_jar = pkg_resources.resource_filename('javaresources', 'python-matsim-instance-1.0-SNAPSHOT-jar-with-dependencies.jar')
-
 
     jpype.addClassPath(python_matsim_jar)
 
@@ -41,5 +45,6 @@ def start_jvm(jvm_path=jpype.get_default_jvm_path(),
     _logger.info('start jvm with classpath {}'.format(jpype.getClassPath()))
 
     jpype.startJVM(jvm_path, "-Djava.class.path=%s" % jpype.getClassPath(), convertStrings=False)
+
 
 
