6a202393e5cafc1bd5d512e5bcc820682f46d4b7
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2019-07-18 11:15:46 +0200

    remove venv creation from script, as handled by makefile

--- java/generate_python_stubs.sh
+++ java/generate_python_stubs.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-virtualenv -p python3.6 venv
 source venv/bin/activate
-pip install mypy-protobuf protobuf
 protoc --proto_path=src/main/protobuf/ --mypy_out=target/generated-sources/python/ src/main/protobuf/*.proto
